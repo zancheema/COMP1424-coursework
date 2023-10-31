7875df483eb068eb534d5e903871143c9e4075a2
Michi Basnet <sb4010q@greenwich.ac.uk>
2023-10-31 11:39:52 +0000

    created ScheduleActivity.java

---
+++ UniversalYogaAdmin/app/src/main/java/com/zancheema/comp1424/universalyoga/universalyoga/admin/ScheduleActivity.java
@@ -0,0 +1,14 @@
+package com.zancheema.comp1424.universalyoga.universalyoga.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScheduleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule);
+    }
+}
